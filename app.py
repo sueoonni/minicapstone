038aef79496bd302d8009b842c88ead66d3c3429
sueoonni <jong050645@gmail.com>
2025-11-04 20:04:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def get_latest_control():
         return jsonify({"message": "No control commands yet."}), 404
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
